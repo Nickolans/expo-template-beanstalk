1a3966ebd8982d584d01a19afbc4a3b70bd2063a
Nickolans Griffith <nickolansgriffith@gmail.com>
2022-05-07 09:37:27 -0500

    updated beanstalk

--- beanstalk.sh
+++ beanstalk.sh
@@ -4,33 +4,15 @@
 git branch -m master main
 
 # 2. Configure project
+yarn
 yarn eject
 
 # 3. Swap out files
-find ./ios -name '*AppDelegate.m*'
-read fileToReplace
-cp -f "./.swappable/AppDelegate.m" $fileToReplace
 cp -f "./.swappable/README.md" "README.md"
+mv .swappable/.gitignore .
 
 # 4. Clean up
-rm -f beanstalk.h
-rm -f ".swappable/"
-
-tput setaf 2; echo "========================================\n\nYour beanstalk has grown :)\n\n========================================"
-
-# Script to configure project
-
-# 1. Git
-git branch -m master main
-
-# 2. Configure and swap
-yarn eject || find ./ios -name '*AppDelegate.m*' || read fileToReplace || cp -f "./.swappable/AppDelegate.m" $fileToReplace
-
-# 3. Swap README
-cp -f "./.swappable/README.md" "README.md"
-
-# 4. Clean up
-rm -f beanstalk.h
-rm -f ".swappable/"
+rm -f beanstalk.sh
+rm -rf .swappable
 
 tput setaf 2; echo "========================================\n\nYour beanstalk has grown :)\n\n========================================"
